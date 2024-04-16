7231ae68bf508c1042e4168a65a9c0f3043fa499
Santi0628 <santiago.gc0628@gmail.com>
2024-04-16 00:56:08 -0500

    V1 del seguimiento

---
+++ MetodosOrdenamiento/Generar.java
@@ -0,0 +1,75 @@
+package MetodosOrdenamiento;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Generar 
+{
+   
+    public static void main(String[] args) 
+    {
+         int[] casos = {40000000, 50000000};
+
+         for (int i : casos)
+         {
+             generar(i, "arreglo_" + i);
+         }
+    }
+
+    private static void generar (int size, String file)
+    {
+        Random r = new Random();
+        
+        try (FileWriter fileWriter = new FileWriter(getPath("/arreglos/" + file + ".txt"))) {
+            for (int i = 0; i < size; i++) {
+                fileWriter.write(r.nextInt(10000000, 99999999 + 1) + ";");
+                System.out.println("Creando: " + i + "/" + size);
+            }
+            System.out.println("¡HECHO!\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static int[] cargar_arreglo (String file)
+    {
+        List<String> lines = new ArrayList<>();
+        
+        try (BufferedReader reader = new BufferedReader(new FileReader(getPath("/arreglos/" + file + ".txt")))) {
+            
+            String line;
+            while ((line = reader.readLine()) != null) {
+                lines.add(line);
+            }
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        String texto = lines.get(0);
+        String[] values = texto.split(";");
+        int[] arreglo = new int[values.length];
+
+        for (int i = 0; i < values.length; i++) {
+
+            arreglo[i] = Integer.parseInt(values[i]);
+        }
+        
+        return arreglo;
+    }
+    
+    private static String getPath ( String ruta )
+	{
+		String raiz = System.getProperty("user.dir");
+		String filePath = raiz + "/" + ruta;
+		return filePath;
+	}
+}
+
---
+++ MetodosOrdenamiento/TreeSort.java
@@ -0,0 +1,97 @@
+package MetodosOrdenamiento;
+
+// Java program to
+// implement Tree Sort
+class TreeSort {
+
+    // Class containing left and
+    // right child of current
+    // node and key value
+    class Node
+    {
+        int key;
+        Node left, right;
+
+        public Node(int item)
+        {
+            key = item;
+            left = right = null;
+        }
+    }
+
+    // Root of BST
+    Node root;
+
+    // Constructor
+    TreeSort()
+    {
+        root = null;
+    }
+
+    // This method mainly
+    // calls insertRec()
+    void insert(int key)
+    {
+        root = insertRec(root, key);
+    }
+
+    /* A recursive function to
+    insert a new key in BST */
+    Node insertRec(Node root, int key)
+    {
+
+		/* If the tree is empty,
+		return a new node */
+        if (root == null)
+        {
+            root = new Node(key);
+            return root;
+        }
+
+		/* Otherwise, recur
+		down the tree */
+        if (key < root.key)
+            root.left = insertRec(root.left, key);
+        else if (key > root.key)
+            root.right = insertRec(root.right, key);
+
+        /* return the root */
+        return root;
+    }
+
+    // A function to do
+    // inorder traversal of BST
+    void inorderRec(Node root)
+    {
+        if (root != null)
+        {
+            inorderRec(root.left);
+            inorderRec(root.right);
+        }
+    }
+    void treeins(int arr[])
+    {
+        for(int i = 0; i < arr.length; i++)
+        {
+            insert(arr[i]);
+        }
+
+    }
+
+    // Driver Code
+    public static void main(String[] args)
+    {
+        TreeSort tree = new TreeSort();
+        int[] arreglo= Generar.cargar_arreglo("arreglo_60000000");
+
+        long startTime = System.nanoTime(); // Capturing start time
+        tree.treeins(arreglo);
+        tree.inorderRec(tree.root);
+        long endTime = System.nanoTime(); // Capturing end time
+        double duration = (endTime - startTime) / 1_000_000_000.0; // Calculating duration in seconds
+        System.out.println("Time taken for sorting: " + duration + " seconds");
+    }
+}
+
+// This code is contributed
+// by Vibin M
